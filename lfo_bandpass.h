ea8df7a848ccaded476b9e59e03bd03bac79fecc
Timothy Krause <tmkrs4482@gmail.com>
2024-12-12 15:05:13 -0700

    Added a license.

--- lfo_bandpass.h
+++ lfo_bandpass.h
@@ -1,3 +1,28 @@
+/*
+
+twk.so is a set of LADSPA plugins.
+
+Copyright 2024 Tim Krause
+
+This file is part of twk.so.
+
+twk.so is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+
+twk.so is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with twk.so. If not, see
+<https://www.gnu.org/licenses/>.
+
+Contact: tim.krause@twkrause.ca
+
+*/
 #include <ladspa.h>
 
 extern LADSPA_Descriptor LFOBandpass_Descriptor;
--- lfr_bandpass5.c
+++ lfr_bandpass5.c
@@ -1,4 +1,29 @@
 /*
+
+twk.so is a set of LADSPA plugins.
+
+Copyright 2024 Tim Krause
+
+This file is part of twk.so.
+
+twk.so is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+
+twk.so is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with twk.so. If not, see
+<https://www.gnu.org/licenses/>.
+
+Contact: tim.krause@twkrause.ca
+
+*/
+/*
  * LADSPA bandpass filter bank of 5 with random frequency modulation
  * 
  * fc = center frequency
